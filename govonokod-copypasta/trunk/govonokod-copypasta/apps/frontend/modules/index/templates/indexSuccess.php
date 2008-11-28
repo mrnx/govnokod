05928e6eeeb9566e352e8934de521b124499a895
korenskoy <korenskoy@a31cdd40-adb3-11dd-98eb-e31df2311c8a>
2008-11-28 13:00:30 +0000

    Маленько изменил стили

--- govonokod-copypasta/trunk/govonokod-copypasta/apps/frontend/modules/index/templates/indexSuccess.php
+++ govonokod-copypasta/trunk/govonokod-copypasta/apps/frontend/modules/index/templates/indexSuccess.php
@@ -1,10 +1,14 @@
 <form action="<?php echo url_for('index/index') ?>" method="post">
-<table>
+<table class="w100">
+  <colgroup>
+    <col style="width: 130px;" />
+    <col/>
+  </colgroup>
   <tfoot>
     <tr>
       <td colspan="2">
-        <label><input type="checkbox" name="remember" <?php echo ($remember != null ? 'checked="checked"' : '')?> /> <?php echo __('Remember me')?></label>
-        <input type="submit" value="<?php echo  __('Submit') ?>" />
+        <label><input type="checkbox" id="remember" name="remember" <?php echo ($remember != null ? 'checked="checked"' : '')?> /> <?php echo __('Remember me')?></label><br />        
+        <input type="submit" value="<?php echo  __('Submit') ?>" id="submit" />
       </td>
     </tr>
   </tfoot>
--- govonokod-copypasta/trunk/govonokod-copypasta/apps/frontend/modules/index/templates/itemSuccess.php
+++ govonokod-copypasta/trunk/govonokod-copypasta/apps/frontend/modules/index/templates/itemSuccess.php
@@ -3,12 +3,16 @@
 
 <h2 class="green"><?php echo __('Submit a correction or amendment below.')?></h2>
 <form action="<?php echo url_for('index/index') ?>" method="post">
-<table>
+<table class="w100">
+  <colgroup>
+    <col style="width: 130px;" />
+    <col/>
+  </colgroup>
   <tfoot>
     <tr>
       <td colspan="2">
-        <label><input type="checkbox" name="remember" <?php echo ($remember != null ? 'checked="checked"' : '')?> /> <?php echo __('Remember me')?></label>
-        <input type="submit" value="Отправить" />
+        <label><input type="checkbox" id="remember"  name="remember" <?php echo ($remember != null ? 'checked="checked"' : '')?> /> <?php echo __('Remember me')?></label><br/>
+        <input type="submit" value="Отправить" id="submit" />
       </td>
     </tr>
   </tfoot>
