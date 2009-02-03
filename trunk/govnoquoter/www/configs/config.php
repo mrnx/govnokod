31cb47a2d303b9bda31cdfa7f1a5ad83053d2c49
wiistriker <wiistriker@a31cdd40-adb3-11dd-98eb-e31df2311c8a>
2009-02-03 04:29:30 +0000

    сокращение количества запросов к БД

--- trunk/govnoquoter/www/configs/config.php
+++ trunk/govnoquoter/www/configs/config.php
@@ -23,7 +23,7 @@ define('SITE_PATH', '');
 define('COOKIE_DOMAIN', '');
 
 define('DEBUG_MODE', 1);
-define('SYSTEM_PATH', realpath(dirname(__FILE__) . '/../../../mzz0.2/system/'));
+define('SYSTEM_PATH', realpath(dirname(__FILE__) . '/../../../mzz_stable/system/'));
 
 /**
  * Идентификатор записи в БД для неавторизированных пользователей
