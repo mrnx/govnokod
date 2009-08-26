6b7f7f827deeb0ac2e0766423cd0fc14138b3359
wiistriker <wiistriker@a31cdd40-adb3-11dd-98eb-e31df2311c8a>
2009-08-26 06:53:47 +0000

    mspookie

--- govnoquoter/mspookie/mspookie.rb
+++ govnoquoter/mspookie/mspookie.rb
@@ -1,21 +1,30 @@
 #MSpookie for govnokod.ru
 #MSpookie is a simple mail pool server
+#really
 
 #Required
 require "mysql"
+require "base64"
 require "net/smtp"
 
 #Connects to server
-connection = Mysql.connect "localhost", "root", "***", "govnokod"
+connection = Mysql.connect "localhost", "root", "", "govnokod"
+
+connection.query "SET NAMES `utf8`"
 
 #Gets set of elements
-set = connection.query "SELECT * FROM `mailer_mail`"
+set = connection.query "SELECT * FROM `mailer_mail` WHERE `id`"
 
 if set != nil and set.num_rows > 0
     smtp = Net::SMTP.start('localhost', 25)
-
     while row = set.fetch_hash do
-        smtp.send_message "From: #{row['fromName']} <#{row['from']}>\nTo: #{row['toName']} <#{row['to']}>\nSubject:#{row['subject']}\n\n#{row['body']}", #Message text
+        fromHeader = row['fromName'] ? row['fromName'] + ' <' + row['from'] + '>' : row['from'];
+        toHeader = row['toName'] ? row['toName'] + ' <' + row['to'] + '>' : row['to']
+        subjectHeader = '=?UTF-8?B?' + Base64.encode64(row['subject']) + '?='
+    
+        body = Base64.encode64(row['body'])
+    
+        smtp.send_message "From: #{fromHeader}\nTo: #{toHeader}\nSubject: #{subjectHeader}\nContent-type: text/html;charset=UTF-8\nContent-Transfer-Encoding: base64\n\n#{body}", #Message text
             row["from"], #From field
             [row["to"]] #To array (may be many entries)
     end
