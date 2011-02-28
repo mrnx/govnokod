d1121faaaeb9d6b01cb0fa13177633bcd224bbef
korenskoy <korenskoy@gmail.com>
2011-02-28 11:12:36 +0000

    Initialize QuestionBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,7 +19,8 @@ class AppKernel extends Kernel
             //new Symfony\Bundle\DoctrineMongoDBBundle\DoctrineMongoDBBundle(),
 
             // register your bundles
-            new Sensio\HelloBundle\HelloBundle(),
+            new Govnokod\QuestionBundle\QuestionBundle(),
+            // new Sensio\HelloBundle\HelloBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
@@ -39,11 +40,5 @@ class AppKernel extends Kernel
         // use YAML for configuration
         // comment to use another configuration format
         $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
-
-        // uncomment to use XML for configuration
-        //$loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.xml');
-
-        // uncomment to use PHP for configuration
-        //$loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.php');
     }
 }
